86907326dce09edb0c3f14c8ae3c89fd79f37700
bortim <borti@live.de>
2014-11-21 22:43:41 +0100

    Update account_roles.sql

--- datenbank/account_roles.sql
+++ datenbank/account_roles.sql
@@ -2,5 +2,5 @@ CREATE TABLE account_roles
 (
     RoleId TINYINT UNSIGNED NOT NULL,
     AccountId INT UNSIGNED NOT NULL,
-    FOREIGN KEY (Id) REFERENCES accounts
+    FOREIGN KEY (AccountId) REFERENCES accounts
 )
