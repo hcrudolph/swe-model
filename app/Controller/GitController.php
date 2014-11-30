83ffaa403a990578578fa98d0b8dbbd68fcc6e13
bortim <borti@live.de>
2014-11-30 19:29:54 +0100

    Update GitController.php

--- app/Controller/GitController.php
+++ app/Controller/GitController.php
@@ -6,11 +6,11 @@ class GitController extends AppController {
   public function pull()
   {
     $this->autoRender = false;
-    echo getcwd()."<br />";
+    echo getcwd();
     echo shell_exec("git pull");
     echo "<br />";
     echo "Hello World!<br />";
-    echo shell_exec("echo 'hallo'");
+    echo shell_exec("echo 'hello from Console'");;
   }
 }
 ?>
