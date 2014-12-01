cd5a13a492fe63a70c98c3dbe8f8fc6105ffdf66
bortim <borti@live.de>
2014-12-01 08:55:00 +0100

    Update ModeController.php
    Controller for Safe-Mode check

--- app/Controller/ModeController.php
+++ app/Controller/ModeController.php
@@ -1,7 +1,14 @@
 <?php
 
-App::uses('AppController', 'Controller');class ModeController extends AppController {      public function safe()    {      $this->autoRender = false;      echo getcwd()."<br />"; 
-     if(ini_get('safe_mode')){echo 'safe-mode is on';}else{echo 'safe-mode is off';}
-       } 
-
+App::uses('AppController', 'Controller');
+class ModeController extends AppController {
+     public function safe()
+     {
+          $this->autoRender = false;
+          if(ini_get('safe_mode')){
+               echo 'safe-mode is on';
+          }else{
+               echo 'safe-mode is off';
+          }
+     }
 ?>
