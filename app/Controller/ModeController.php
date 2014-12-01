ff449aa0fc80ea6d226e81994beef46722a70997
bortim <borti@live.de>
2014-12-01 08:44:54 +0100

    Create ModeController.php

---
+++ app/Controller/ModeController.php
@@ -0,0 +1,7 @@
+<?php
+
+App::uses('AppController', 'Controller');class GitController extends AppController {      public function pull()    {      $this->autoRender = false;      echo getcwd()."<br />"; 
+     echo exec("git pull")."<br />"; 
+    echo exec("whoami");    } 
+
+?>
