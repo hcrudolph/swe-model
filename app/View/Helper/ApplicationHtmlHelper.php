01db7e6e5573b6175404c25b6d04774bde9e40d4
bortim <borti@live.de>
2014-12-03 10:07:03 +0100

    Update and rename HtmlAppHelper.php to ApplicationHtmlHelper.php

--- app/View/Helper/ApplicationHtmlHelper.php
+++ app/View/Helper/ApplicationHtmlHelper.php
@@ -1,5 +1,5 @@
 App::uses('HtmlHelper', 'View/Helper');
-class HtmlAppHelper extends HtmlHelper {
+class ApplicationHtmlHelper extends HtmlHelper {
 
     $_tags['polymertemplate'] = '<link rel="import" href="%s">';
 
