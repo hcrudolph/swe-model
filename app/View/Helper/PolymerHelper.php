32d3f3ce16622a5ff4980c72866dad13235e4cff
bortim <borti@live.de>
2014-12-03 11:03:52 +0100

    Update PolymerHelper.php

--- app/View/Helper/PolymerHelper.php
+++ app/View/Helper/PolymerHelper.php
@@ -1,6 +1,7 @@
 <?php
 //App::uses('HtmlHelper', 'View/Helper');
 class PolymerHelper extends AppHelper {
+	var $helpers = array('Html');
 /*
  * Add polymer-template
 */
@@ -22,7 +23,26 @@ class PolymerHelper extends AppHelper {
 		$url = $this->assetUrl($path, array('pathPrefix' => 'polymer/'));
 	}
 	return sprintf('<link rel="import" href="%s">', $url);
-	//return sprintf($this->_tags['polymertemplate'], $url);
+    }
+    
+    public function js($path) {
+        //Load path-array
+        if(is_array($path))
+        {
+            foreach ($path as $pathValue)
+            {
+                this.polymer($pathValue);
+            }
+            return;
+        }
+        
+        //Create Import-URL
+        if (strpos($path, '//') !== false) {
+		$url = $path;
+	} else {
+		$url = $this->assetUrl($path, array('pathPrefix' => 'polymer/'));
+	}
+	return $this->Html->script($url);
     }
 }
 ?>
