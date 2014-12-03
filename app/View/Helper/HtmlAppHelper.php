d0a21115171278e00bdc1b08d3c5f588243ce939
bortim <borti@live.de>
2014-12-03 09:45:32 +0100

    Update HtmlAppHelper.php
    Added helper for polymertemplates

--- app/View/Helper/HtmlAppHelper.php
+++ app/View/Helper/HtmlAppHelper.php
@@ -1,4 +1,28 @@
 App::uses('HtmlHelper', 'View/Helper');
 class HtmlApplicationHelper extends HtmlHelper {
-    // Add your code to override the core HtmlHelper
+
+    $_tags['polymertemplate'] = '<link rel="import" href="%s">';
+
+/*
+ * Add polymer-template
+*/
+    public function polymerTemplate($path) {
+        //Load path-array
+        if(is_array($path))
+        {
+            foreach ($path as $pathValue)
+            {
+                this.polymer($pathValue);
+            }
+            return;
+        }
+        
+        //Create Import-URL
+        if (strpos($path, '//') !== false) {
+			$url = $path;
+		} else {
+			$url = $this->assetUrl($path, array('pathPrefix' => 'polymer'));
+		}
+		return sprintf($this->_tags['polymertemplate'], $url);
+    }
 }
