dcbb60e116ba457f7c2efa4dc0dfd8ae66b0bafd
bortim <borti@live.de>
2014-12-04 10:31:18 +0100

    Update GitController.php

--- app/Controller/GitController.php
+++ app/Controller/GitController.php
@@ -6,8 +6,11 @@ class GitController extends AppController {
 
   public function pull()
   {
-    $this->autoRender = false;
-    echo getcwd()."<br />";
+  $this->autoRender = false;
+    echo '<table>';
+    echo "<tr><td>current directory</td><td>";
+    echo getcwd()."</td></tr>";
+    echo "<tr><td>git pull</td><td>";
     $output = array();
     exec("git pull 2>&1", $output);
     foreach($output as $line)
@@ -15,7 +18,50 @@ class GitController extends AppController {
       echo $line;
       echo '<br />';
     }
-    echo exec("whoami");
+    echo "</td></tr>";
+    
+    echo "<tr><td>current user</td><td>".exec("whoami")."</td></tr>";
+    echo '</table>';
   }
+  public function overwrite()
+  {
+    $this->autoRender = false;
+    echo '<table>';
+    echo "<tr><td>current directory</td><td>";
+    echo getcwd()."</td></tr>";
+    echo "<tr><td>git fetch all</td><td>";
+    $output = array();
+    exec("git fetch all 2>&1", $output);
+    foreach($output as $line)
+    {
+      echo $line;
+      echo '<br />';
+    }
+    echo "</td></tr>";
+    
+    echo "<tr><td>git fetch all</td><td>";
+    $output = array();
+    exec("git fetch all 2>&1", $output);
+    foreach($output as $line)
+    {
+      echo $line;
+      echo '<br />';
+    }
+    echo "</td></tr>";
+    
+    echo "<tr><td>git reset --hard origin/development</td><td>";
+    $output = array();
+    exec("git reset --hard origin/development 2>&1", $output);
+    foreach($output as $line)
+    {
+      echo $line;
+      echo '<br />';
+    }
+    echo "</td></tr>";
+    
+    echo "<tr><td>current user</td><td>".exec("whoami")."</td></tr>";
+    echo '</table>';
+  }
+  
 }
 ?>
