8cd09af0c642148b30f32f098a9b647c7746bb27
bortim <borti@live.de>
2014-12-22 12:23:15 +0100

    [Post] Vorbereitung der Validierung

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -50,24 +50,24 @@ class Post extends AppModel {
 		),
 		/*'visiblebegin' => array(
 			'date' => array(
-				'rule' => array('date'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
+				'rule' => array('date', 'dmy'),
+				'message' => 'Falsches Datenformat',
+				'allowEmpty' => false,
+				'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 		'visibleend' => array(
 			'date' => array(
-				'rule' => array('date'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
+				'rule' => array('date', 'dmy'),
+				'message' => 'Falsches Datenformat',
+				'allowEmpty' => true,
+				'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-		),*/
+		)*/
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
