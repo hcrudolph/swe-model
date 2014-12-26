24350d9a09800da631893c06cc8a5b6769f915b4
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2014-12-26 18:17:28 +0100

    see previous..

--- datenbank/schema_261214.sql
+++ datenbank/schema_261214.sql
@@ -1,11 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 26. Dez 2014 um 18:16
+-- Server Version: 5.6.20
+-- PHP-Version: 5.5.15
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Datenbank: `fitness`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `accounts`
+--
 
 CREATE TABLE IF NOT EXISTS `accounts` (
 `id` int(10) unsigned NOT NULL,
@@ -15,11 +34,23 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `created` datetime DEFAULT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `accounts_certificates`
+--
+
 CREATE TABLE IF NOT EXISTS `accounts_certificates` (
 `id` int(10) unsigned NOT NULL,
   `certificate_id` int(10) unsigned NOT NULL,
   `account_id` int(10) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `accounts_dates`
+--
 
 CREATE TABLE IF NOT EXISTS `accounts_dates` (
 `id` int(10) unsigned NOT NULL,
@@ -27,12 +58,24 @@ CREATE TABLE IF NOT EXISTS `accounts_dates` (
   `account_id` int(10) unsigned NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `accounts_trainings`
+--
+
 CREATE TABLE IF NOT EXISTS `accounts_trainings` (
 `id` int(10) unsigned NOT NULL,
   `account_id` int(10) unsigned NOT NULL,
   `downloadlink` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bills`
+--
 
 CREATE TABLE IF NOT EXISTS `bills` (
 `id` int(10) unsigned NOT NULL,
@@ -43,21 +86,37 @@ CREATE TABLE IF NOT EXISTS `bills` (
   `payed` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `certificates`
+--
+
 CREATE TABLE IF NOT EXISTS `certificates` (
 `id` int(10) unsigned NOT NULL,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `courses`
+--
+
 CREATE TABLE IF NOT EXISTS `courses` (
 `id` int(10) unsigned NOT NULL,
   `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `category` smallint(5) unsigned DEFAULT NULL,
-  `maxcount` int(10) unsigned DEFAULT NULL,
-  `mincount` int(10) unsigned DEFAULT NULL,
   `description` text COLLATE utf8_unicode_ci,
   `created` datetime DEFAULT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `dates`
+--
 
 CREATE TABLE IF NOT EXISTS `dates` (
 `id` int(10) unsigned NOT NULL,
@@ -69,6 +128,12 @@ CREATE TABLE IF NOT EXISTS `dates` (
   `mincount` int(10) unsigned DEFAULT NULL,
   `maxcount` int(10) unsigned DEFAULT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `people`
+--
 
 CREATE TABLE IF NOT EXISTS `people` (
 `id` int(10) unsigned NOT NULL,
@@ -85,6 +150,12 @@ CREATE TABLE IF NOT EXISTS `people` (
   `birthdate` date DEFAULT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `posts`
+--
+
 CREATE TABLE IF NOT EXISTS `posts` (
 `id` int(10) unsigned NOT NULL,
   `account_id` int(10) unsigned NOT NULL,
@@ -95,10 +166,22 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `visibleend` date DEFAULT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `rooms`
+--
+
 CREATE TABLE IF NOT EXISTS `rooms` (
 `id` int(10) unsigned NOT NULL,
   `name` varchar(10) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `tariffs`
+--
 
 CREATE TABLE IF NOT EXISTS `tariffs` (
 `id` int(10) unsigned NOT NULL,
@@ -107,92 +190,194 @@ CREATE TABLE IF NOT EXISTS `tariffs` (
   `term` smallint(5) unsigned NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `accounts`
+--
 ALTER TABLE `accounts`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
 
+--
+-- Indexes for table `accounts_certificates`
+--
 ALTER TABLE `accounts_certificates`
  ADD PRIMARY KEY (`id`), ADD KEY `certificate_id` (`certificate_id`), ADD KEY `account_id` (`account_id`);
 
+--
+-- Indexes for table `accounts_dates`
+--
 ALTER TABLE `accounts_dates`
  ADD PRIMARY KEY (`id`), ADD KEY `date_id` (`date_id`), ADD KEY `account_id` (`account_id`);
 
+--
+-- Indexes for table `accounts_trainings`
+--
 ALTER TABLE `accounts_trainings`
  ADD PRIMARY KEY (`id`), ADD KEY `account_id` (`account_id`);
 
+--
+-- Indexes for table `bills`
+--
 ALTER TABLE `bills`
  ADD PRIMARY KEY (`id`), ADD KEY `account_id` (`account_id`), ADD KEY `tariff_id` (`tariff_id`);
 
+--
+-- Indexes for table `certificates`
+--
 ALTER TABLE `certificates`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
 
+--
+-- Indexes for table `courses`
+--
 ALTER TABLE `courses`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
 
+--
+-- Indexes for table `dates`
+--
 ALTER TABLE `dates`
  ADD PRIMARY KEY (`id`), ADD KEY `course_id` (`course_id`), ADD KEY `room_id` (`room_id`), ADD KEY `director` (`director`);
 
+--
+-- Indexes for table `people`
+--
 ALTER TABLE `people`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `account_id` (`account_id`);
 
+--
+-- Indexes for table `posts`
+--
 ALTER TABLE `posts`
  ADD PRIMARY KEY (`id`), ADD KEY `account_id` (`account_id`);
 
+--
+-- Indexes for table `rooms`
+--
 ALTER TABLE `rooms`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
 
+--
+-- Indexes for table `tariffs`
+--
 ALTER TABLE `tariffs`
  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `accounts`
+--
 ALTER TABLE `accounts`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `accounts_certificates`
+--
 ALTER TABLE `accounts_certificates`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `accounts_dates`
+--
 ALTER TABLE `accounts_dates`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `accounts_trainings`
+--
 ALTER TABLE `accounts_trainings`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bills`
+--
 ALTER TABLE `bills`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `certificates`
+--
 ALTER TABLE `certificates`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `courses`
+--
 ALTER TABLE `courses`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `dates`
+--
 ALTER TABLE `dates`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `people`
+--
 ALTER TABLE `people`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `posts`
+--
 ALTER TABLE `posts`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `rooms`
+--
 ALTER TABLE `rooms`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tariffs`
+--
 ALTER TABLE `tariffs`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- Constraints der exportierten Tabellen
+--
 
+--
+-- Constraints der Tabelle `accounts_certificates`
+--
 ALTER TABLE `accounts_certificates`
 ADD CONSTRAINT `accounts_certificates_ibfk_1` FOREIGN KEY (`certificate_id`) REFERENCES `certificates` (`id`),
 ADD CONSTRAINT `accounts_certificates_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
 
+--
+-- Constraints der Tabelle `accounts_dates`
+--
 ALTER TABLE `accounts_dates`
 ADD CONSTRAINT `accounts_dates_ibfk_1` FOREIGN KEY (`date_id`) REFERENCES `dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `accounts_dates_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints der Tabelle `accounts_trainings`
+--
 ALTER TABLE `accounts_trainings`
 ADD CONSTRAINT `accounts_trainings_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
 
+--
+-- Constraints der Tabelle `bills`
+--
 ALTER TABLE `bills`
 ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`),
 ADD CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`tariff_id`) REFERENCES `tariffs` (`id`);
 
+--
+-- Constraints der Tabelle `dates`
+--
 ALTER TABLE `dates`
 ADD CONSTRAINT `dates_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `dates_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON UPDATE CASCADE,
 ADD CONSTRAINT `dates_ibfk_3` FOREIGN KEY (`director`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints der Tabelle `people`
+--
 ALTER TABLE `people`
 ADD CONSTRAINT `people_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints der Tabelle `posts`
+--
 ALTER TABLE `posts`
 ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
 
