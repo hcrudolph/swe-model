00e46f706bb5f4b655f3f902d9929cfca6a2c071
StephanWagener <stephan-wagener@web.de>
2014-12-27 17:51:25 +0100

    Im Studiomanagement wurde ein Dummy für die Rechnungsdaten eingefügt. Demnach fehlt noch das Befüllen mit den richtigen Usern

--- app/Controller/BillsController.php
+++ app/Controller/BillsController.php
@@ -21,6 +21,14 @@ class BillsController extends AppController {
  * @return void
  */
 	public function index() {
+        if($this->request->is('ajax'))
+        {
+            $this->layout = 'ajax';
+        } else
+        {
+            throw new AjaxImplementedException;
+        }
+
 		$this->Bill->recursive = 0;
 		$this->set('bills', $this->Paginator->paginate());
 	}
