ebedae32963bdd5d09507dda8d374b87662296d7
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2014-12-29 15:15:40 +0100

    rebake Tariff model

--- app/Model/Tariff.php
+++ app/Model/Tariff.php
@@ -3,22 +3,22 @@ App::uses('AppModel', 'Model');
 /**
  * Tariff Model
  *
- * @property Bill $Bill
+ * @property Course $Course
  */
 class Tariff extends AppModel {
 
-/**
- * Display field
- *
- * @var string
- */
+	/**
+	 * Display field
+	 *
+	 * @var string
+	 */
 	public $displayField = 'description';
 
-/**
- * Validation rules
- *
- * @var array
- */
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
 	public $validate = array(
 		'description' => array(
 			'notEmpty' => array(
@@ -31,26 +31,8 @@ class Tariff extends AppModel {
 			),
 		),
 		'amount' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
 			'numeric' => array(
 				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'term' => array(
-			'naturalNumber' => array(
-				'rule' => array('naturalNumber'),
 				//'message' => 'Your custom message here',
 				//'allowEmpty' => false,
 				//'required' => false,
@@ -70,25 +52,18 @@ class Tariff extends AppModel {
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * hasAndBelongsToMany associations
- *
- * @var array
- */
-	public $hasAndBelongsToMany = array(
-		'Bill' => array(
-			'className' => 'Bill',
-			'joinTable' => 'bills_tariffs',
-			'foreignKey' => 'tariff_id',
-			'associationForeignKey' => 'bill_id',
-			'unique' => 'keepExisting',
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
+	public $belongsTo = array(
+		'Course' => array(
+			'className' => 'Course',
+			'foreignKey' => 'course_id',
 			'conditions' => '',
 			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'finderQuery' => '',
+			'order' => ''
 		)
 	);
-
 }
