b530f664632ecf1fdba681ef76f3fdb3b96668dc
bortim <borti@live.de>
2015-01-01 14:32:44 +0100

    [EmailController] Controller zum Senden von Emails für Testzwecke

---
+++ app/Controller/EmailController.php
@@ -0,0 +1,23 @@
+<?php
+set_time_limit(0);
+App::uses('AppController', 'Controller');
+
+class EmailController extends AppController {
+
+    public function send()
+    {
+        $this->autoRender = false;
+        $email = new CakeEmail('noreplay');
+        $email->to('Empfänger');
+        $email->subject('[Abgesagt] Kurs#1 (Schwierigkeitsgrad: 1) am 11.01.2015');
+        $email->send("Hier steht ihre Nachricht!");
+    }
+
+    //Test benötigt keinen Login
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+        $this->Auth->allow();
+    }
+}
+?>
