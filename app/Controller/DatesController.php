0e0c41f79dc72420c9e6de8f75c3c81b26044865
bortim <borti@live.de>
2015-01-04 10:33:22 +0100

    [DatesController][signoffUser] Anpassung des Email-Textes

--- app/Controller/DatesController.php
+++ app/Controller/DatesController.php
@@ -332,6 +332,23 @@ class DatesController extends AppController {
                         $answer['success'] = true;
                         $answer['message'] = "Sie wurden erfolgreich abgemeldet";
                         $answer['courseId'] = $date['Date']['course_id'];
+
+                        if(!empty($this->Auth->user['Person']['email'])) {
+                            $email = new CakeEmail('noreplay');
+                            $email->viewVars(array(
+                                'nachname' => $this->Auth->user['Person']['name'],
+                                'vorname' => $this->Auth->user['Person']['surname'],
+                                'dateBegin' => $date['Date']['begin'],
+                                'courseName' => $date['Course']['name'],
+                                'courseLevel' => $date['Course']['level'],
+
+                            ));
+                            $email-> template('Dates/signoffUser');
+                            $email->emailFormat('text');
+                            $email->to($this->Auth->user['Person']['email']);
+                            $email->subject('[Abgemeldet]'.$date['Course']['name'].' (Schwierigkeitsgrad: '+$date['Course']['level'].') am '+ date('d.m.Y', strtotime($date['Date']['begin'])));
+                            $email->send();
+                        }
                     } else {
                         $answer['success'] = false;
                         $answer['message'] = "Sie konnten nicht abgemeldet werden";
