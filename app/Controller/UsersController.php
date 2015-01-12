7e80a1e613d3df98403ae960d80b0c78bc626edb
rodichsv <sven.rodich@gmx.de>
2015-01-12 22:15:23 +0100

    Issue #102 - Login/Logout Handling
    deny any
    allow login/checklogin
    alles weitere wird über rollen geregelt.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -323,4 +323,104 @@ class UsersController extends AppController
             throw new AjaxImplementedException;
         }
     }
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+        $this->Auth->deny();
+        $this->Auth->allow('login', 'checklogin');
+
+    }
+
+    /**
+     * login method
+     *
+     * @throws AjaxImplementedException, MethodNotAllowedException
+     * @param string $id
+     * @return void
+     */
+    public function login() {
+        if($this->request->is('ajax')) {
+            if ($this->request->is('post')) {
+                $this->autoRender = false;
+                $this->layout=null;
+                $this->response->type('json');
+                $answer = array();
+                if ($this->Auth->login()) {
+                    $view = new View($this, false);
+                    $answer['login'] = true;
+                    $answer['message'] = 'Sie wurden erfolgreich eingeloggt';
+                    $answer['logout'] = $view->element('authentification/logout', array('user' => $this->Auth->user()));
+                    $answer['sidebar'] = $view->element('sidebar', array('user' => $this->Auth->user()));
+                } else
+                {
+                    $answer['login'] = false;
+                    $answer['message'] = 'Sie konnten nicht eingeloggt werden';
+                }
+                echo json_encode($answer);
+            } else {
+                throw new MethodNotAllowedException;
+            }
+        }  else {
+            throw new AjaxImplementedException;
+        }
+    }
+
+    /**
+     * checklogin method
+     *
+     * @throws AjaxImplementedException
+     * @return void
+     */
+    public function checklogin() {
+        if($this->request->is('ajax'))
+        {
+            $this->autoRender = false;
+            $this->layout=null;
+            $this->response->type('json');
+            $answer = array();
+            if($this->Auth->loggedIn()) {
+                $answer['loggedin'] = true;
+            } else {
+                $answer['loggedin'] = false;
+            }
+            echo json_encode($answer);
+        } else {
+            throw new AjaxImplementedException;
+        }
+    }
+
+    /**
+     * logout method
+     *
+     * @throws AjaxImplementedException, MethodNotAllowedException
+     * @param string $id
+     * @return void
+     */
+    public function logout() {
+        if($this->request->is('ajax')) {
+            if ($this->request->is('post')) {
+                $this->autoRender = false;
+                $this->layout=null;
+                $this->response->type('json');
+                $answer = array();
+                if ($this->Auth->logout()) {
+                    $view = new View($this, false);
+                    $answer['logout'] = true;
+                    $answer['message'] = 'Sie wurden erfolgreich ausgeloggt';
+                    $answer['login'] = $view->element('authentification/login');
+                    $answer['sidebar'] = $view->element('sidebar');
+                } else
+                {
+                    $answer['logout'] = false;
+                    $answer['message'] = 'Sie konnten nicht ausgeloggt werden';
+                }
+                echo json_encode($answer);
+            } else {
+                throw new MethodNotAllowedException;
+            }
+
+        } else {
+            throw new AjaxImplementedException;
+        }
+    }
 }
